3959b771952c285dea963173a6c113cafea49d8d
Jeremy Du <97752691+Razieldu@users.noreply.github.com>
2023-10-06 15:12:26 +0800

    10/6
    補回失去的程式碼

--- changeExcelCellValue.py
+++ changeExcelCellValue.py
@@ -13,3 +13,10 @@ sheet = workbook.Sheets("UserData")
 # value = sheet.Cells(1, 1).Value
 accountAndPassword = input("輸入以作為帳號及密碼__ ")
 name = input("輸入姓名__")
+
+sheet.Cells(2, 1).Value = accountAndPassword
+sheet.Cells(2, 2).Value = accountAndPassword
+sheet.Cells(2, 3).Value = name
+# 保存工作簿并关闭Excel
+workbook.Save()
+excel.Quit()
